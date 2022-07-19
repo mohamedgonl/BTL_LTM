32731cebee4d0732ec500efa18563b34c9f0bb83
long <long.nq.hust@gmail.com>
2022-07-19 23:52:02 +0700

    done 5

--- Server/Controllers.h
+++ Server/Controllers.h
@@ -111,6 +111,94 @@ queue<char*> recvStreamProcessing(LoginSession &loginSession, char buff[BUFF_SIZ
 	return statements;
 }
 
+	// 2. Login
+	string loginAccount(string username, string password, UserInfo* userInfo) {
+
+	// check if account is logged in
+	for (int i = 0; i < sizeof(loginSessions); i++) {
+		if (!strcmp((loginSessions[i]->userInfo.username).c_str, username.c_str)) return "113";
+	}
+	// if this account has not logged in
+	for (int i = 0; i < sizeof(accounts); i++) {
+		if (!strcmp(accounts[i].username.c_str, username.c_str)) {
+			if (!strcmp(accounts[i].password.c_str, password.c_str)) { // if username and password matched
+																	   // update user info
+				userInfo->username = username;
+				userInfo->status = 1;
+				return "110";
+			}
+			else return "112";
+		}
+	}
+	return "111";
+}
+
+// 3. Register
+
+string registerAccount(string username, string password) {
+	for (int i = 0; i < sizeof(accounts); i++) {
+		if (!strcmp(accounts[i].username.c_str, username.c_str)) // if username is existed
+			return "121";
+	}
+
+	// save account to data file
+
+	try
+	{
+		ofstream file(fileDirectory);
+		file << username << " " << password<<"\n";
+
+	}
+	catch (const std::exception&)
+	{
+		cout << "Error at function 3.Register: Save data error\n";
+		return "122";
+	}
+	return "120";
+}
+
+// 4. Get list all teams
+
+string getAllTeams(UserInfo* userInfo) {
+
+	// login check
+	if (userInfo->status == 0) return "211";
+
+	string response= "210|";
+	for (int i = 0; i < sizeof(teams); i++) {
+		response += std::to_string(teams[i]->id);
+		response += " "+teams[i]->name;
+		response += " "+ std::to_string(sizeof (teams[i]->members)) +"|";
+	}
+	return response;
+}
+
+// 5. Join team
+
+string joinTeam(UserInfo* userInfo, int teamId) {
+	for (int i = 0; i < sizeof(teams); i++) {
+		if (teams[i]->id == teamId) {
+			if (sizeof(teams[i]->members) < 3) {
+				string s = "230|" + userInfo->username;
+				char * _s = s.c_str;
+				// send join request to team leader
+				Send((teams[i]->members)[0]->socketInfo.connSocket, _s,sizeof(_s),0);
+				return "220";
+			}
+			else return "221";
+		}
+	}
+	// teamId not existed
+	return "222";
+}
+
+//6. create team
+string createTeam(UserInfo* userInfo, string teamName) {
+	Team newTeam;
+	for(int i =0 ; i< MAX_TEAM)
+
+}
+
 #endif // !Handle user statement
 
 
